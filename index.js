0b9d039cd693e52f358ab5b69bbc24d45ae351f2
Helder Pereira <helder.pereira@xing.com>
2020-10-28 18:40:15 +0100

    now some express boilerplate

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log('HI!');
+const express = require('express')
+
+const app = express()
+const port = process.env.PORT || 3000
+
+app.get('/', function(req, res) {
+    res.json({
+        status: 'ok',
+        description: `it's ok what else can I say...`
+    })
+})
+
+app.listen(port, function() {
+    console.log(`running on port ${port}`)
+})
