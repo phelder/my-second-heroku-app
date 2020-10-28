b9c1cbc826f9fe92ea49ced23c59fe80e2ce4272
Helder Pereira <helder.pereira@xing.com>
2020-10-28 20:17:27 +0100

    add dbtest endpoint

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 require('dotenv').config()
 
 const express = require('express')
+const mysql = require('mysql2')
 
 const app = express()
 const port = process.env.PORT
+
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
+})
 
 app.get('/', function(req, res) {
 
@@ -12,6 +20,15 @@ app.get('/', function(req, res) {
     res.json({
         status: 'ok',
         description: `it's ok what else can I say...`
+    })
+})
+
+app.get('/dbtest', function(req, res) {
+
+    connection.query(`SELECT * FROM offices`, function(error, result) {
+    
+        res.json(result)
+    
     })
 })
 
