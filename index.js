df3493a20169c23e80ff16a30e5251d517e6a724
Helder Pereira <phelder@gmail.com>
2021-02-06 12:03:53 +0100

    cors

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
 const express = require('express')
+const cors = require('cors')
 const bodyParser = require('body-parser')
 const offices = require('./offices')
 
@@ -7,6 +8,7 @@ const app = express()
 const port = process.env.PORT
 
 app.use(bodyParser.json())
+app.use(cors())
 
 app.get('/', function (req, res) {
     console.log(req.hostname);
